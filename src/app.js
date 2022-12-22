65f14137d738cf7201809fd0f00898de6ce7c461
Onur <onurozet@hotmail.com>
2022-12-22 19:11:27 +0300

    JavaScriptFirstLesson

--- src/app.js
+++ src/app.js
@@ -22,15 +22,30 @@ console.log(euroDun)
 let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
 
 console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
+for (let i = 0; i < konutKredileri.length; i++) {
+   
     console.log("<li>"+konutKredileri[i]+"</li>")
+    
 }
 console.log("</ul>")
 
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
 
-console.log(konutKredileri)
+
+
+
+
+
+
+// console.log("<ul>")
+// for(let i = 0;i<konutKredileri.length;i++){
+//     console.log("<li>"+konutKredileri[i]+"</li>")
+// }
+// console.log("</ul>")
+
+// {/* <ul>
+//     <li>Konut kredisi</li>
+//     <li>Emlak Konut Kredisi</li>
+//     <li>Kamu Konut Kredisi</li>
+// </ul> */}
+
+// console.log(konutKredileri)
